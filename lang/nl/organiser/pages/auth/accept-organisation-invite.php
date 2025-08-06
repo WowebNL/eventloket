328cceedfbd2265f15b2b15c3b8d02385fa3ec91
Michel Verhoeven <michel@woweb.nl>
2025-08-06 10:56:32 +0200

    Added password rules info to organisator register + text changes invite mail

--- lang/nl/organiser/pages/auth/accept-organisation-invite.php
+++ lang/nl/organiser/pages/auth/accept-organisation-invite.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'heading' => 'Uitnodiging voor organisatie accepteren',
+    'heading' => 'Uitnodiging om binnen je organisatie toegevoegd te worden aan EventLoket accepteren',
     'subheading' => 'Maak je account aan om de uitnodiging te accepteren',
 
     'button' => 'Uitnodiging accepteren',
