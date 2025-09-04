8cdb87e0fa07eb76182e593c5e80e103fd748017
Michel Verhoeven <michel@woweb.nl>
2025-09-04 09:43:59 +0200

    Fix: show correct headings for invites

--- lang/nl/municipality/pages/auth/accept-municipality-invite.php
+++ lang/nl/municipality/pages/auth/accept-municipality-invite.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'heading' => 'Uitnodiging voor :role accepteren',
+    'heading' => 'Uitnodiging voor de rol als :role accepteren',
     'subheading' => 'Maak je account aan om de uitnodiging te accepteren',
 
     'button' => 'Uitnodiging accepteren',
