e28cb128f16592acf00b389822a064d3c11dc9e6
Michel-Verhoeven <info@woweb.nl>
2025-11-12 21:50:41 +0100

    Added endpoint to check events by period and municipality

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\EventsController;
 use App\Http\Controllers\Api\FormSessionController;
 use App\Http\Controllers\Api\LocationServerController;
 use App\Http\Controllers\Api\MunicipalityVariableController;
@@ -10,6 +11,7 @@ use Laravel\Passport\Http\Middleware\EnsureClientIsResourceOwner;
 
 Route::group(['middleware' => [EnsureClientIsResourceOwner::class, NormalizeOpenformsInput::class]], function () {
     Route::post('/locationserver/check', [LocationServerController::class, 'check'])->name('api.locationserver.check');
+    Route::post('/events/check', [EventsController::class, 'check'])->name('api.events.check');
     Route::get('/formsessions', FormSessionController::class)->name('api.formsessions.check');
     Route::post('/open-notifications/listen', [OpenNotificationsController::class, 'listen'])->name('api.open-notifications.listen');
     Route::get('/municipality-variables/{municipality:brk_identification}', MunicipalityVariableController::class)->name('api.municipality-variables');
