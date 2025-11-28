a69b57f00c2849f94a8d27d7f70dc3ca2444e334
Lorenso D’Agostino <git@lorenso.nl>
2025-11-28 10:01:39 +0200

    Removes AdminUserResource import on advisor calendar page
    This was somehow causing the filters not to work on the advisor
    calendar page.
    
    And this resource definition is not needed

--- app/Filament/Advisor/Pages/Calendar.php
+++ app/Filament/Advisor/Pages/Calendar.php
@@ -2,14 +2,11 @@
 
 namespace App\Filament\Advisor\Pages;
 
-use App\Filament\Admin\Resources\AdminUserResource;
 use App\Filament\Advisor\Widgets\AdvisorCalendarWidget;
 use App\Filament\Shared\Pages\Calendar as BaseCalendar;
 
 class Calendar extends BaseCalendar
 {
-    protected static string $resource = AdminUserResource::class;
-
     protected function getHeaderWidgets(): array
     {
         return [
