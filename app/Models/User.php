faec4751b3a969171020b6e21d4d33fdbf5e23ea
Michel-Verhoeven <info@woweb.nl>
2025-12-16 15:53:25 +0100

    Fix: error setting role

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,6 +54,7 @@ class User extends Authenticatable implements HasAppAuthentication, HasAppAuthen
         'email_verified_at',
         'phone',
         'password',
+        'role',
     ];
 
     /**
