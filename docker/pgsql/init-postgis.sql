c6bc262b7575a2851b01001204c241a6b0b78397
Dion Snoeijen <hallo@dionsnoeijen.nl>
2026-04-08 16:30:11 +0200

    Feat: Docker setup for Open Forms Eventloket auth plugin
    - Add custom Dockerfile for Open Forms that installs the
      openforms-eventloket-auth plugin from a sibling directory
    - Update docker-compose-full-architecture.yml to build custom
      Open Forms image with plugin environment variables
    - Fix init-postgis.sql to work with dynamic database names
    - Add OPEN_FORMS_TOKEN_SIGNING_KEY to .env.example

--- docker/pgsql/init-postgis.sql
+++ docker/pgsql/init-postgis.sql
@@ -1,9 +1,6 @@
--- Install PostGIS extension in the main database
-\c evenement_applicatie;
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS postgis_topology;
-
--- Install PostGIS extension in testing database as well
+-- PostGIS extensions for the testing database
+-- Note: The testing database is created by Sail's 10-create-testing-database.sql
+-- The main database gets PostGIS from the postgis/postgis image's 10_postgis.sh
 \c testing;
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS postgis_topology;
