090f48174cd6d2534810de275c78188d388d6fec
Dion Snoeijen <hallo@dionsnoeijen.nl>
2026-04-08 16:55:40 +0200

    Fix indenting

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use Laravel\Passport\Http\Middleware\EnsureClientIsResourceOwner;
 Route::group(['middleware' => [EnsureClientIsResourceOwner::class, NormalizeOpenformsInput::class]], function () {
     Route::post('/locationserver/check', [LocationServerController::class, 'check'])->name('api.locationserver.check');
     Route::post('/events/check', [EventsController::class, 'check'])->name('api.events.check');
-Route::get('/validate-eventloket-token', ValidateEventloketTokenController::class)->name('api.validate-eventloket-token');
+    Route::get('/validate-eventloket-token', ValidateEventloketTokenController::class)->name('api.validate-eventloket-token');
     Route::post('/open-notifications/listen', [OpenNotificationsController::class, 'listen'])->name('api.open-notifications.listen');
     Route::get('/municipality-variables/{municipality:brk_identification}', MunicipalityVariableController::class)->name('api.municipality-variables');
 });
