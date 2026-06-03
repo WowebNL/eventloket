c94c8c956641736d201b59962b7ec8adc7e3a8ed
Michel-Verhoeven <info@woweb.nl>
2026-06-03 15:24:14 +0200

    Feat: added Horizon for Queue monitoring

--- config/services.php
+++ config/services.php
@@ -33,6 +33,7 @@ return [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
+        'horizon_webhook_url' => env('HORIZON_SLACK_WEBHOOK_URL'),
     ],
 
     'open_forms' => [
